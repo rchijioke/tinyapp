dd6789cc765231c57951e9f20f425637e3cb93ea
Roy <ekeneroy@gmail.com>
2023-04-19 13:41:47 -0500

    Switching to Encrypted Cookies

--- express_server.js
+++ express_server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const router = express.Router();
-var cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const cookieSession = require('cookie-session')
+
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+keys: ['key1', 'key2']
+}));
 
 const urlDatabase = {
   b6UTxQ: {
@@ -77,7 +82,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const userId = req.cookies.user_id;
+  const userId = req.session.user_id;
   if (!isLoggedIn(userId)) {
     res.status(401).send("<html><body>Please <a href='/login'>log in</a> to view URLs.</body></html>");
   } else {
@@ -90,20 +95,32 @@ app.get("/urls", (req, res) => {
 }
 });
 
+// app.get("/urls/new", (req, res) => {
+//   // res.render("urls_new", { user: undefined });
+// });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", { user: undefined });
+  const user = users[req.session.user_id];
+  const userid = req.session.user_id
+  if(!isLoggedIn(userid)) {
+    return res.redirect("/login") 
+  }
+  res.render("urls_new", {user});
 });
-
 app.get("/urls/:id", (req, res) => {
-  const userId = req.cookies.user_id
+  const userId = req.session.user_id
   const shortUrl = req.params.id;
   const url = urlDatabase[shortUrl];
+  console.log("url Object",url)
+    console.log("cookie",userId)
+    console.log("short Url", shortUrl)
+    console.log("url Database", urlDatabase)
   if (!isLoggedIn(userId)) {
     res.status(401).send("<html><body>Please <a href='/login'>log in</a> to view this URL.</body></html>");
-  } else if (!url || url.userId !== userId) {
+  } else if (!url || url.userID !== userId) {
+    
     res.status(404).send("<html><body>URL not found or you do not have permission to view this URL.</body></html>");
   } else {
-  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl].longURL };
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl].longURL, user: users[userId] };
   res.render("urls_show", templateVars);
   }
 });
@@ -131,7 +148,7 @@ app.get("/register", (req, res) => {
   //const user = users[req.cookies.user_id]
   // console.log("user", user)
   // const templateVars = { user };
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   if (isLoggedIn(userid)) {
     res.redirect("/urls");
   } else {
@@ -142,7 +159,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   if (isLoggedIn(userid)) {
    return res.redirect("/urls")
   }
@@ -150,23 +167,19 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars); // Render the login form template
 });
 
-app.get("/urls/new", (req, res) => {
-  const user = req.user;
-  const userid = req.cookies.user_id
-  if(!isLoggedIn(userid)) {
-    return res.redirect("/login") 
-  }
-  res.render("urls_new", user);
-});
 
 app.post("/urls", (req, res) => {
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   if(!isLoggedIn(userid)){
     res.send("Please <a href='/login'> log in </a> to create URL")
   }
   const shortUrl = generateRandomString();
   const longUrl = req.body.longURL;
-  urlDatabase[shortUrl] = longUrl;
+  urlDatabase[shortUrl] = {
+    longURL: longUrl,
+    userID: userid
+  };
+
   res.redirect(`/urls/${shortUrl}`);
 });
 
@@ -188,10 +201,10 @@ app.post("/login", (req, res) => {
   const user = findUserByEmail(email);
   if (!user) {
     res.status(403).send("User not Found");
-  } else if (user.password !== password) {
-    res.status(403).send("User not Found");
+  } else if (!bcrypt.compareSync(password, user.password)) {
+    res.status(403).send("Incorrect password");
   } else {
-    res.cookie("user_id", user.id);
+    req.session.user_id = user.id;
     // const userId = req.cookies.user_id;
     // const templateVars = {
     //   user: users[userId],
@@ -203,11 +216,12 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/login");
 });
 
 app.post("/register", (req, res) => {
+  
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -217,14 +231,15 @@ app.post("/register", (req, res) => {
   if (findUserByEmail(email)) {
     return res.status(400).send("Email is already in use.");
   }
+  const hashedPassword = bcrypt.hashSync(password, 10)
   const userId = generateRandomString();
   const newUser = {
     id: userId,
     email: email,
-    password: password,
+    password: hashedPassword,
   };
   users[userId] = newUser;
-  res.cookie("user_id", userId);
+  req.session.user_id = userId;
   res.redirect("/urls");
 });
 
